4758a925a1e054bbafc9f805d462a0121eb192fd
Keihong <keihong@gmail.com>
2026-04-17 01:51:47 +0800

    Initial Commit

---
+++ GymFlow/GymFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymFlowApp.swift
+//  GymFlow
+//
+//  Created by 簡奇峰 on 2026/4/17.
+//
+
+import SwiftUI
+
+@main
+struct GymFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
